6bf0d00d335f9f9ea52356b55df15401e68b7293
lubnabeary <157459046+lubnabeary@users.noreply.github.com>
2026-05-06 20:18:54 -0700

    Configure base path and allowed hosts in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mydesignsystem/',
   plugins: [react()],
+  server: {
+    allowedHosts: ['twig-passport-scarring.ngrok-free.dev']
+  }
 })
